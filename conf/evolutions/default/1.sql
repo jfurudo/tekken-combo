a50a3c4cbea75b4a756ab43dc5ae29419b7d0621
jfurudo <jfurudo1@gmail.com>
2015-06-03 12:02:36 +0900

    added schema recipes and recipes_moves

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -15,3 +15,22 @@ note VARCHAR(255));
 
 # --- !Downs
 DROP TABLE moves;
+
+
+# --- !Ups
+create table recipes (
+id bigint auto_increment not null primary key,
+author bigint not null
+);
+
+# --- !Downs
+drop table recipes;
+
+# --- !Ups
+create table recipes_moves (
+move_id int not null,
+recipe_id bigint not null
+);
+
+# --- !Downs
+drop table recipe_move;
